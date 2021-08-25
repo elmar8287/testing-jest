5a2b9e12d2fbe562d68ce4a00514119e28a09ece
elmar8287 <L-mar@inbox.ru>
2021-08-25 17:01:20 +0400

    Done all necessery tests

--- calculator.test.js
+++ calculator.test.js
@@ -1,29 +1,29 @@
-const calculator = require('./calculator.js');
+const Calculator = require('./calculator');
 
 describe('Unit tests', () => {
   test('add method', () => {
-    const calc = new calculator();
+    const calc = new Calculator();
     calc.add(5);
     calc.add(5);
     calc.add(6);
     expect(calc.value).toBe(16);
   });
   test('subtract method', () => {
-    const calc = new calculator();
+    const calc = new Calculator();
     calc.add(6);
     calc.subtract(1);
     calc.subtract(3);
     expect(calc.value).toBe(2);
   });
   test('multiple method', () => {
-    const calc = new calculator();
+    const calc = new Calculator();
     calc.add(6);
     calc.subtract(3);
     calc.multiply(4);
     expect(calc.value).toBe(16);
   });
   test('divide method', () => {
-    const calc = new calculator();
+    const calc = new Calculator();
     calc.add(14);
     calc.divide(7);
     expect(calc.value).toBe(2);
